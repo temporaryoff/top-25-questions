d4a8eabf48400cf25051b775e3877889ffbf8bc0
yogeshkumar patil <ybp345@gmail.com>
2023-12-31 15:06:52 +0530

    top 25 questions first

---
+++ top25questions.js
@@ -0,0 +1,14 @@
+
+
+
+        var str = "saare jaha se achha hindustaan humara";
+
+        var savedStr = str.split(" ")
+        .map(function(wrod){
+           return word.split("").reverse().join("")
+        })
+
+        console.log(savedStr.join(" "));
+
+
+
